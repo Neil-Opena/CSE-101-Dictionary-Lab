9062ca2097d9cbea9ddf0c6382c75f11e363be4b
Neil Opena <neil1114o@gmail.com>
2016-11-01 21:07:36 -0400

    11 -1
    created list a

--- Lab 8.py
+++ Lab 8.py
@@ -3,5 +3,13 @@
 def buildIndex(text):
     #creates an empty dictionary to hold final result
     dictionary = {}
-    
+    words = text.split(' ')
+    mylist = []
+    mylist.append(words)
+    print (mylist)
     #uses split() to break into list of words
+
+
+text = input("Enter text here")
+buildIndex(text)
+
