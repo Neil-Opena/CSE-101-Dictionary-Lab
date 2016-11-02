80af6df17e60b4a67d76c168af9b0ff40b0bdcf2
Neil Opena <neil1114o@gmail.com>
2016-11-01 22:37:29 -0400

    Added main( )
    -added main function
    -added FIX ME for errors
    -fixed return/print for displayIndex funciton

--- Lab 8.py
+++ Lab 8.py
@@ -12,16 +12,24 @@ def buildIndex(text):
         else:
             dictionary[word] = list()
     return dictionary
+    #FIX ME
+    #doesnt include the first occurance of word
+    #repeats the same position ex: 9,9,9
 
 def displayIndex(dictionary):
-    keys = sorted(dictionary.keys())
-    return keys
-    """for key in keys:"""
+    sorted_keys = sorted(dictionary.keys())
+    for key in sorted_keys:
+        print (key,':',dictionary[key])
+    
+
+def main():
+    text = input("Enter text here: ")
+    dictionary = buildIndex(text)
+    #print (dictionary)
+    print(displayIndex(dictionary)) 
 
 
-text = input("Enter text here:")
-dictionary = (buildIndex(text))
-print (dictionary) 
-print(displayIndex(dictionary)) 
+main()
+ 
 
 
